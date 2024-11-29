e9b356d1c8bfdd6bc4f13a2456d85dfbf986bf92
Jakob Hoeg <jakmoe01@easv365.dk>
2024-11-29 09:07:04 +0100

    added #16 - #35

--- src/common/pokemon-data.ts
+++ src/common/pokemon-data.ts
@@ -86,24 +86,164 @@ export const POKEMON_DATA: { [key: string]: PokemonConfig } = {
     possibleColors: [PokemonColor.default]
   },
   weedle: {
-    id: 12,
+    id: 13,
     name: 'Weedle',
     generation: PokemonGeneration.Gen1,
     cry: 'Weedle!',
     possibleColors: [PokemonColor.default]
   },
   kakuna: {
-    id: 12,
+    id: 14,
     name: 'Kakuna',
     generation: PokemonGeneration.Gen1,
     cry: 'Kakuna!',
     possibleColors: [PokemonColor.default]
   },
   beedrill: {
-    id: 12,
+    id: 15,
     name: 'Beedrill',
     generation: PokemonGeneration.Gen1,
     cry: 'Beedrill!',
+    possibleColors: [PokemonColor.default]
+  },
+  pidgey: {
+    id: 16,
+    name: 'Pidgey',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Pidgey!',
+    possibleColors: [PokemonColor.default]
+  },
+  pidgeotto: {
+    id: 17,
+    name: 'Pidgeotto',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Pidgeotto!',
+    possibleColors: [PokemonColor.default]
+  },
+  pidgeot: {
+    id: 18,
+    name: 'Pidgeot',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Pidgeot!',
+    possibleColors: [PokemonColor.default]
+  },
+  rattata: {
+    id: 19,
+    name: 'Rattata',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Rattata!',
+    possibleColors: [PokemonColor.default]
+  },
+  raticate: {
+    id: 20,
+    name: 'Raticate',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Raticate!',
+    possibleColors: [PokemonColor.default]
+  },
+  spearow: {
+    id: 21,
+    name: 'Spearow',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Spearow!',
+    possibleColors: [PokemonColor.default]
+  },
+  fearow: {
+    id: 22,
+    name: 'Fearow',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Fearow!',
+    possibleColors: [PokemonColor.default]
+  },
+  ekans: {
+    id: 23,
+    name: 'Ekans',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Ekans!',
+    possibleColors: [PokemonColor.default]
+  },
+  arbok: {
+    id: 24,
+    name: 'Arbok',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Arbok!',
+    possibleColors: [PokemonColor.default]
+  },
+  pikachu: {
+    id: 25,
+    name: 'Pikachu',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Pikachu!',
+    possibleColors: [PokemonColor.default]
+  },
+  raichu: {
+    id: 26,
+    name: 'Raichu',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Raichu!',
+    possibleColors: [PokemonColor.default]
+  },
+  sandshrew: {
+    id: 27,
+    name: 'Sandshrew',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Sandshrew!',
+    possibleColors: [PokemonColor.default]
+  },
+  nidoran_female: {
+    id: 28,
+    name: 'Nidoran♀',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Nidoran!',
+    possibleColors: [PokemonColor.default]
+  },
+  nidorina: {
+    id: 29,
+    name: 'Nidorina',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Nidorina!',
+    possibleColors: [PokemonColor.default]
+  },
+  nidoqueen: {
+    id: 30,
+    name: 'Nidoqueen',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Nidoqueen!',
+    possibleColors: [PokemonColor.default]
+  },
+  nidoran_male: {
+    id: 31,
+    name: 'Nidoran♂',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Nidoran!',
+    possibleColors: [PokemonColor.default]
+  },
+  nidorino: {
+    id: 32,
+    name: 'Nidorino',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Nidorino!',
+    possibleColors: [PokemonColor.default]
+  },
+  nidoking: {
+    id: 33,
+    name: 'Nidoking',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Nidoking!',
+    possibleColors: [PokemonColor.default]
+  },
+  clefairy: {
+    id: 34,
+    name: 'Clefairy',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Clefairy!',
+    possibleColors: [PokemonColor.default]
+  },
+  clefable: {
+    id: 35,
+    name: 'Clefable',
+    generation: PokemonGeneration.Gen1,
+    cry: 'Clefable!',
     possibleColors: [PokemonColor.default]
   },
   dragonite: {
