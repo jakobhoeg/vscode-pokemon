ff08126f33bc16470a176a203b513fc8cc6a4ce4
Kasper Wissendorf <kw2011@live.dk>
2026-01-11 11:14:28 +0100

    chore: ran prettier to get everything up to date

--- src/common/names.ts
+++ src/common/names.ts
@@ -1,9 +1,9 @@
 import { POKEMON_NAMES } from '../panel/pokemon';
 
 export function randomName(): string {
-    const collection: ReadonlyArray<string> = POKEMON_NAMES;
+	const collection: ReadonlyArray<string> = POKEMON_NAMES;
 
-    return (
-        collection[Math.floor(Math.random() * collection.length)] ?? 'Unknown'
-    );
+	return (
+		collection[Math.floor(Math.random() * collection.length)] ?? 'Unknown'
+	);
 }
